42c54dc413c51564bda0d6148a7bd7e9105e3afd
deckerh <washulil@aim.com>
2014-11-01 14:40:44 -0700

    Added add_visit_attribute_type.story
    Added add_visit_attribute_type.story, put additional steps into
    VisitSteps.java

--- release-test/src/main/java/org/openmrs/steps/VisitSteps.java
+++ release-test/src/main/java/org/openmrs/steps/VisitSteps.java
@@ -13,6 +13,7 @@
  */
 package org.openmrs.steps;
 
+import java.util.List;
 import java.util.Random;
 
 import org.jbehave.core.annotations.Given;
@@ -54,6 +55,48 @@ public class VisitSteps extends Steps {
 	public void enterDescriptionAsTheDescription(String description) {
 		WebElement textarea = getWebDriver().findElement(By.name("description"));
 		textarea.sendKeys(description);
+	}
+	
+	@When("I enter $value for Min Occurs")
+	public void enterValueForMinOccurs(String value) {
+		type(value, into(textbox().with(attribute("name", equalTo("minOccurs")))));
+	}
+	
+	@When("I enter $value for Max Occurs")
+	public void enterValueForMaxOccurs(String value) {
+		type(value, into(textbox().with(attribute("name", equalTo("maxOccurs")))));
+	}
+	
+	@When("I select $value as the Datatype")
+	public void selectValueAsTheDatatype(String value) {
+		WebElement datatypeCombobox = getWebDriver().findElement(By.name("datatypeClassname"));
+		List<WebElement> options = datatypeCombobox.findElements(By.tagName("option"));
+		for (WebElement option : options) {
+	        if(value.equals(option.getText()))
+	            option.click();
+	    }
+	}
+	
+	@When("I enter $value as the Datatype Configuration")
+	public void enterValueAsTheDatatypeConfiguration(String value) {
+		WebElement textarea = getWebDriver().findElement(By.name("datatypeConfig"));
+		textarea.sendKeys(value);
+	}
+	
+	@When("I enter $value as the Handler Configuration")
+	public void enterValueAsTheHandlerConfiguration(String value) {
+		WebElement textarea = getWebDriver().findElement(By.name("handlerConfig"));
+		textarea.sendKeys(value);
+	}
+	
+	@When("I select $value as the Preferred Handler")
+	public void selectValueAsThePreferredHandler(String value) {
+		WebElement datatypeCombobox = getWebDriver().findElement(By.name("preferredHandlerClassname"));
+		List<WebElement> options = datatypeCombobox.findElements(By.tagName("option"));
+		for (WebElement option : options) {
+	        if(value.equals(option.getText()))
+	            option.click();
+	    }
 	}
 	
 	@Then("take me to $title as heading")
